fcd575fa96ba00cf080601a75cd35df490a3d370
Mansoo Kim <mansoo0621@mju.ac.kr>
2020-05-30 13:29:08 +0900

    Add Model <Info>

--- page/models.py
+++ page/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Info(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    title = models.CharField(max_length = 20)
+    address = models.CharField(max_length = 255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.image, self.title, self.address
